8aca8e1f561cf81b3d57b552973c07db64c1e2bc
Zalida-Khan <zkhan64@my.bcit.ca>
2024-12-10 19:59:30 -0800

    changing vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('@tensorflow/tfjs') || id.includes('@tensorflow-models/mobilenet')) {
-            return 'tensorflow';
-          }
-        }
-      }
-    }
-  }
 });
 
